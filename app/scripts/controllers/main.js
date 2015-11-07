b539b3d43ffeeeebafeb12f9d0328865c0646b37
Gift Kantika <kantikagift@gmail.com>
2015-11-07 11:49:31 +0700

    Fix bug of fade out hilight div when click back

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,7 @@ angular.module('chessmateApp')
       renderBoard($scope.game.boards[0]);
     });
 
-    $scope.$on('animation-completed', function (event,isMoveForward) {
+    $scope.$on('animation-completed', function (event,isMoveForward,isPlay) {
       if($scope.interval){
         clearInterval($scope.interval);
       }
@@ -31,13 +31,22 @@ angular.module('chessmateApp')
       }
 
       renderBoard(nextBoard);
+
+      if(isPlay) {
+        setTimeout(function(){
+          $rootScope.$broadcast('play', null);
+        }, 1000);
+      }
     });
 
     $scope.$on('next', function (event) {
-      move($scope.currentBoard, true);
+      move($scope.currentBoard, true, null);
+    });
+    $scope.$on('play', function (event) {
+      move($scope.currentBoard, true, true);
     });
     $scope.$on('back', function (event) {
-      move($scope.currentBoard, false);
+      move($scope.currentBoard, false, null);
     });
     $scope.$on('add-comment', function (event, comment) {
       $scope.currentBoard.comment = comment;
@@ -45,6 +54,9 @@ angular.module('chessmateApp')
 
     $scope.next = function(){
       $rootScope.$broadcast('next', null);
+    };
+    $scope.play = function(){
+      $rootScope.$broadcast('play', null);
     };
     $scope.back = function(){
       $rootScope.$broadcast('back', null);
@@ -55,7 +67,7 @@ angular.module('chessmateApp')
       renderBoard(board);
     };
 
-    function move(currentBoard, isMoveForward) {
+    function move(currentBoard, isMoveForward, isPlay) {
       var source = null;
       var destination = null;
 
@@ -72,25 +84,30 @@ angular.module('chessmateApp')
 
       var desinationPosition = $("#" + destination).position();
       var sourcePosition = $("#" + source).position();
+
       var piece = $("#" + source).children();
 
       var destinationClass = buildCss(desinationPosition.left - sourcePosition.left, desinationPosition.top - sourcePosition.top);
 
       //remove highlight
       $('.piece').removeClass('highlight');
+      $('.piece').removeClass('destination');
 
-      // move
-      piece.css(destinationClass);
+      setTimeout(function(){
+        // move
+        $(".board div").addClass("divTransition");
+        piece.css(destinationClass);
+      }, 100);
 
       var currentTurn = currentBoard.turn;
       $scope.interval = setInterval(function(){
         if($scope.currentBoard.turn == currentTurn){
-          $rootScope.$broadcast('animation-completed', isMoveForward);
+          $rootScope.$broadcast('animation-completed', isMoveForward, isPlay);
         }
-      },2500);
+      },1500);
 
       $(piece).on("webkitTransitionEnd otransitionEnd oTransitionEnd msTransitionEnd transitionEnd",function(event) {
-        $rootScope.$broadcast('animation-completed', isMoveForward);
+        $rootScope.$broadcast('animation-completed', isMoveForward, isPlay);
       });
     };
 
